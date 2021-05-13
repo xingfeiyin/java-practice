de399fd2c6f933950540f50df2cf370fa2719739
xingfeiyin <xingfeiyin@creditease.cn>
2021-05-13 10:47:55 +0800

    spi

--- src/main/java/com/yinxf/java/spi/AobingMain.java
+++ src/main/java/com/yinxf/java/spi/AobingMain.java
@@ -6,7 +6,11 @@ import java.util.ServiceLoader;
 /**
  * @author yinxf
  * @Date 2021/5/7
- * @Description
+ * @Description Java SPI机制
+ * Java SPI在查找扩展实现类的时候遍历SPI配置文件并将实现类全部实例化。
+ * 假如实现类初始化的过程比较耗时，但代码里又用不上它，这就产生了资源的浪费。
+ *
+ *
  **/
 public class AobingMain {
     public static void main(String[] args) {
---
+++ src/main/java/com/yinxf/java/spi/DubboSpi.java
@@ -0,0 +1,20 @@
+package com.yinxf.java.spi;
+
+
+/**
+ * @author yinxf
+ * @Date 2021/5/8
+ * @Description Dubbo SPI
+ *
+ * Dubbo SPI可以按需加载实现类，增加了IOC和AOP的特性，还有自适应扩展机制
+ * Dubbo对配置文件目录的约定：
+ *      META-INF/services/目录 ：该目录下的SPI配置文件是为了用来兼容Java SPI.
+ *      META-INF/dubbo/目录 ：该目录存放用户自定义的SPI配置文件
+ *      META-INF/dubbo/internal/目录 ：该目录存放 Dubbo 内部使用的SPI配置文件
+ *
+ **/
+public class DubboSpi {
+
+    public static void main(String[] args) {
+    }
+}
